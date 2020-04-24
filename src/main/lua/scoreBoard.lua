e760b54f570bcf2f7fb0b166477c34add9e393eb
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-04-24 15:53:58 +0200

    working counter

--- src/main/lua/scoreBoard.lua
+++ src/main/lua/scoreBoard.lua
@@ -1,16 +1,17 @@
 local button_config = {
   counter = {
-    width = 0, height = 0,
-    font = 800,
+    width = 1200, height = 1000,
+    font = 750,
     scale = {0.1, 0.1, 0.1}
   },
   plus_minus = {
-    width = 300, height = 300,
-    font = 560,
+    width = 375, height = 400,
+    font = 600,
     scale = {0.1, 0.1, 0.1}
   }
 }
-local plus_minus_spacing = 0.08
+local board_thickness = 0.1
+local plus_minus_spacing = 0.18
 
 local counters = {}
 
@@ -19,15 +20,15 @@ function onLoad()
 end
 
 function initButtons()
-  setupCounter("testCounter", {x = 0, y = 0.1, z = 0})
+  setupCounter("testCounter", {x = -0.87, y = board_thickness, z = -1.12})
 end
 
 function setupCounter(name, position)
   local input_function_name = "onInput" .. name
-  self.setVar(input_function_name, function(obj, player_clicker_color, input_value, selected) changeCounterValue(name, tonumber(input_value)) end)
+  self.setVar(input_function_name, function (obj, player_clicker_color, input_value, selected) changeCounterValue(name, tonumber(input_value)) end)
 
   local parameters = {
-    index = #counters + 1,
+    index = #counters,
     input_function = input_function_name,
     function_owner = self,
     label = "0",
@@ -36,8 +37,10 @@ function setupCounter(name, position)
     width = button_config["counter"].width,
     height = button_config["counter"].height,
     font_size = button_config["counter"].font,
-    color = {1, 1, 1},
-    validation = 2,  -- Integer
+    color = {1, 1, 1, 0},
+    font_color = {0, 0, 0, 100},
+    alignment = 3,  -- Center
+    validation = 2,  -- 1: None, 2: Integer
     tab = 2  -- Select next input
   }
   self.createInput(parameters)
@@ -48,47 +51,56 @@ function setupCounter(name, position)
 end
 
 function createPlusButton(parentName, position)
-  local click_function_name = "onClickPlus" .. parentName
-  self.setVar(click_function_name, function(obj, player_clicker_color, alt_click) incrementCounter(parentName, alt_click, 1) end)
+  local click_function_name = "plus_function_" .. parentName
+  print(click_function_name)
+  self.setVar(click_function_name, function (obj, player_clicker_color, alt_click) incrementCounter(parentName, alt_click, 1) end)
 
   self.createButton({
     click_function = click_function_name,
     function_owner = self,
     label = "+",
-    position = {position.x + plus_minus_spacing, position.y, position.z},
+    position = {position.x + plus_minus_spacing, position.y, position.z - 0.04},
     scale = button_config["plus_minus"].scale,
     width = button_config["plus_minus"].width,
     height = button_config["plus_minus"].height,
     font_size = button_config["plus_minus"].font,
-    click_function = "onClickPlus"
   })
 end
 
 function createMinusButton(parentName, position)
-  local click_function_name = "onClickMinus" .. parentName
-  self.setVar(click_function_name, function(obj, player_clicker_color, alt_click) changeCounter(parentName, alt_click, -1) end)
+  local click_function_name = "minus_function_" .. parentName
+  self.setVar(click_function_name, function (obj, player_clicker_color, alt_click) incrementCounter(parentName, alt_click, -1) end)
 
   self.createButton({
     click_function = click_function_name,
     function_owner = self,
     label = "-",
-    position = {position.x - plus_minus_spacing, position.y, position.z},
+    position = {position.x - plus_minus_spacing, position.y, position.z - 0.04},
     scale = button_config["plus_minus"].scale,
     width = button_config["plus_minus"].width,
     height = button_config["plus_minus"].height,
     font_size = button_config["plus_minus"].font,
-    click_function = "onClickPlus"
   })
 end
 
-function changeCounterValue(counterName, input_value)
-  local amount = tonumber(input_value)
-  counters[counterName].label = tostring(tonumber(counters[counterName].label) + amount)
-  self.editInput(counters[counterName])
+function changeCounterValue(counter_name, value)
+  counters[counter_name].value = value
 end
 
-function incrementCounter(counterName, alt_click, amount)
-  if alt_click then amount = amount * 10 end
-  counters[counterName].label = tostring(tonumber(counters[counterName].label) + amount)
-  self.editInput(counters[counterName])
+function incrementCounter(counter_name, alt_click, amount)
+  if alt_click then
+    amount = amount * 10
+  end
+  if not counters[counter_name].value then
+    counters[counter_name].value = 0
+  end
+  counters[counter_name].value = tonumber(counters[counter_name].value) + amount
+  if counters[counter_name].value == 0 then
+    counters[counter_name].value = nil
+  end
+  self.editInput(counters[counter_name])
+end
+
+function updateTotal()
+
 end
