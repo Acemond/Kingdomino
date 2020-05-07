a6bdf3500e86e33e24b8e1b9cbb40ebba7717920
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-05-07 15:59:19 +0200

    Moved util methods to table.lua

--- src/main/lua/util/table.lua
+++ src/main/lua/util/table.lua
@@ -1,8 +1,24 @@
-function table.contains(table, value)
-  for _, table_value in pairs(table) do
+function table.contains(tbl, value)
+  for _, table_value in pairs(tbl) do
     if table_value == value then
       return true
     end
   end
   return false
+end
+
+function table.size(tbl)
+  local size = 0
+  for _, _ in pairs(tbl) do
+    size = size + 1
+  end
+  return size
+end
+
+function table.collectValues(tbl)
+  local result = {}
+  for _, value in pairs(tbl) do
+    table.insert(result, value)
+  end
+  return result
 end
