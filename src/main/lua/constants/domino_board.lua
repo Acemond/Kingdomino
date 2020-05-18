7e32c53249fca9e2564563b282fb93b4ce740496
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-05-18 15:38:56 +0200

    Improved Dominoes board effect

--- src/main/lua/constants/domino_board.lua
+++ src/main/lua/constants/domino_board.lua
@@ -1,16 +1,18 @@
 DominoBoardConstants = {}
 DominoBoardConstants.__index = DominoBoardConstants
 
+DominoBoardConstants.right_boards_guids = { "e5b23a", "7a72d1", "174390", "722967" }
 DominoBoardConstants.right_boards = {
-  [3] = { guid = "e5b23a", position = { 5.50, 1.06, 4.4 } },
-  [4] = { guid = "7a72d1", position = { 5.50, 1.06, 3.22 } },
-  [5] = { guid = "174390", position = { 5.50, 1.06, 1.96 } },
-  [8] = { guid = "722967", position = { 5.50, 1.06, -1.66 } }
+  [3] = { guid = DominoBoardConstants.right_boards_guids[1], position = { 5.50, 1.06, 4.4 } },
+  [4] = { guid = DominoBoardConstants.right_boards_guids[2], position = { 5.50, 1.06, 3.22 } },
+  [5] = { guid = DominoBoardConstants.right_boards_guids[3], position = { 5.50, 1.06, 1.96 } },
+  [8] = { guid = DominoBoardConstants.right_boards_guids[4], position = { 5.50, 1.06, -1.66 } }
 }
 
+DominoBoardConstants.left_boards_guids = { "ae485e", "bd95f5", "8c018b", "a391ea" }
 DominoBoardConstants.left_boards = {
-  [3] = { guid = "ae485e", position = { -5.50, 1.06, 4.4 } },
-  [4] = { guid = "bd95f5", position = { -5.50, 1.06, 3.22 } },
-  [5] = { guid = "8c018b", position = { -5.50, 1.06, 1.96 } },
-  [8] = { guid = "a391ea", position = { -5.50, 1.06, -1.66 } }
+  [3] = { guid = DominoBoardConstants.left_boards_guids[1], position = { -5.50, 1.06, 4.4 } },
+  [4] = { guid = DominoBoardConstants.left_boards_guids[2], position = { -5.50, 1.06, 3.22 } },
+  [5] = { guid = DominoBoardConstants.left_boards_guids[3], position = { -5.50, 1.06, 1.96 } },
+  [8] = { guid = DominoBoardConstants.left_boards_guids[4], position = { -5.50, 1.06, -1.66 } }
 }
