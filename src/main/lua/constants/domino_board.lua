6a48cf616eecd73fd501a4070546cc1375793e1d
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-05-18 15:18:09 +0200

    Disabled manual snap if next to snap point (boards)

---
+++ src/main/lua/constants/domino_board.lua
@@ -0,0 +1,16 @@
+DominoBoardConstants = {}
+DominoBoardConstants.__index = DominoBoardConstants
+
+DominoBoardConstants.right_boards = {
+  [3] = { guid = "e5b23a", position = { 5.50, 1.06, 4.4 } },
+  [4] = { guid = "7a72d1", position = { 5.50, 1.06, 3.22 } },
+  [5] = { guid = "174390", position = { 5.50, 1.06, 1.96 } },
+  [8] = { guid = "722967", position = { 5.50, 1.06, -1.66 } }
+}
+
+DominoBoardConstants.left_boards = {
+  [3] = { guid = "ae485e", position = { -5.50, 1.06, 4.4 } },
+  [4] = { guid = "bd95f5", position = { -5.50, 1.06, 3.22 } },
+  [5] = { guid = "8c018b", position = { -5.50, 1.06, 1.96 } },
+  [8] = { guid = "a391ea", position = { -5.50, 1.06, -1.66 } }
+}
