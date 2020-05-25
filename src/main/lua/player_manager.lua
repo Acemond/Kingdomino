0940a9196fa54d2953d6f1ec7c7d9feb3db0d62d
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-05-25 10:42:19 +0200

    PlayerManager stub

---
+++ src/main/lua/player_manager.lua
@@ -0,0 +1,39 @@
+PlayerManager = {}
+PlayerManager.__index = PlayerManager
+
+local seated_players = {
+  White = false,
+  Orange = false,
+  Purple = false,
+  Red = false,
+  Green = false,
+  Pink = false
+}
+local local_players_button_guid = {
+  enable = "4f044d",
+  disable = "d1e47c"
+}
+
+function PlayerManager.load(save_state)
+
+end
+
+function PlayerManager.update()
+
+end
+
+function PlayerManager.save()
+
+end
+
+function PlayerManager.addPlayer(person_color, seat_color)
+
+end
+
+function PlayerManager.removePlayer(seat_color)
+
+end
+
+function PlayerManager.setPlayerCount(count)
+
+end
